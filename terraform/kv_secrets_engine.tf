f36cfc90fad042df2f41a4f6081e884a0bfba1d1
arpan <me@arpanrec.com>
2022-10-03 22:37:00 +0530

    init secrets

--- terraform/kv_secrets_engine.tf
+++ terraform/kv_secrets_engine.tf
@@ -3,9 +3,9 @@ resource "vault_mount" "secret" {
   type        = "kv-v2"
   description = "Secret Data Store"
   options = {
-    version = 2
-    "cas_required" : false,
-    "max_versions" : 20,
-    "delete_version_after" : "0s"
+    version              = 2
+    cas_required         = false
+    max_versions         = 20
+    delete_version_after = "0s"
   }
 }
