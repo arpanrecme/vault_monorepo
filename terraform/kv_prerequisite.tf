443b0c77262fefdf1cdbbdae984b6915bd7aa8b1
arpan <me@arpanrec.com>
2022-10-04 14:27:04 +0530

    vars

--- terraform/kv_prerequisite.tf
+++ terraform/kv_prerequisite.tf
@@ -1,4 +1,4 @@
-resource "vault_kv_secret_v2" "secret" {
+resource "vault_kv_secret_v2" "secret_prerequisite_openssh" {
   mount = vault_mount.secret.path
   name  = "prerequisite/openssh"
   data_json = jsonencode(
@@ -6,6 +6,40 @@ resource "vault_kv_secret_v2" "secret" {
       private_key             = file(var.VAULT_MONO_LOCAL_FILE_OPENSSH_PRIVATE_KEY),
       public_key              = file(var.VAULT_MONO_LOCAL_FILE_OPENSSH_PUBLIC_KEY),
       private_key_passpharase = file(var.VAULT_MONO_LOCAL_FILE_OPENSSH_PASSPHRASE_PRIVATE_KEY)
+    }
+  )
+}
+
+resource "vault_kv_secret_v2" "secret_prerequisite_openpgp" {
+  mount = vault_mount.secret.path
+  name  = "prerequisite/openpgp"
+  data_json = jsonencode(
+    {
+      private_key             = file(var.VAULT_MONO_LOCAL_FILE_OPENPGP_PRIVATE_KEY),
+      public_key              = file(var.VAULT_MONO_LOCAL_FILE_OPENPGP_PUBLIC_KEY),
+      private_key_passpharase = file(var.VAULT_MONO_LOCAL_FILE_OPENPGP_PASSPHRASE_PRIVATE_KEY)
+    }
+  )
+}
+
+resource "vault_kv_secret_v2" "secret_prerequisite_linode" {
+  mount = vault_mount.secret.path
+  name  = "prerequisite/linode"
+  data_json = jsonencode(
+    {
+      LINODE_CLI_PROD_TOKEN = file(var.VAULT_MONO_LOCAL_FILE_LINODE_CLI_PROD_TOKEN)
+    }
+  )
+}
+
+resource "vault_kv_secret_v2" "secret_prerequisite_rootca" {
+  mount = vault_mount.secret.path
+  name  = "prerequisite/rootca"
+  data_json = jsonencode(
+    {
+      private_key             = file(var.VAULT_MONO_LOCAL_FILE_ROOT_CA_PRIVATE_KEY),
+      certificate             = file(var.VAULT_MONO_LOCAL_FILE_ROOT_CA_CERTIFICATE),
+      private_key_passpharase = file(var.VAULT_MONO_LOCAL_FILE_ROOT_CA_PASSPHRASE_PRIVATE_KEY)
     }
   )
 }
--- terraform/variables_prerequisite.tf
+++ terraform/variables_prerequisite.tf
@@ -24,3 +24,66 @@ variable "VAULT_MONO_LOCAL_FILE_OPENSSH_PUBLIC_KEY" {
     error_message = "Missing OPEN SSH Public Key file path"
   }
 }
+
+variable "VAULT_MONO_LOCAL_FILE_OPENPGP_PRIVATE_KEY" {
+  type    = string
+  default = null
+  validation {
+    condition     = length(var.VAULT_MONO_LOCAL_FILE_OPENPGP_PRIVATE_KEY) > 1
+    error_message = "Missing OPEN PGP Private Key file path"
+  }
+}
+
+variable "VAULT_MONO_LOCAL_FILE_OPENPGP_PASSPHRASE_PRIVATE_KEY" {
+  type    = string
+  default = null
+  validation {
+    condition     = length(var.VAULT_MONO_LOCAL_FILE_OPENPGP_PASSPHRASE_PRIVATE_KEY) > 1
+    error_message = "Missing OPEN PGP Private Key password file path"
+  }
+}
+
+variable "VAULT_MONO_LOCAL_FILE_OPENPGP_PUBLIC_KEY" {
+  type    = string
+  default = null
+  validation {
+    condition     = length(var.VAULT_MONO_LOCAL_FILE_OPENPGP_PUBLIC_KEY) > 1
+    error_message = "Missing OPEN PGP Public Key file path"
+  }
+}
+
+variable "VAULT_MONO_LOCAL_FILE_LINODE_CLI_PROD_TOKEN" {
+  type    = string
+  default = null
+  validation {
+    condition     = length(var.VAULT_MONO_LOCAL_FILE_LINODE_CLI_PROD_TOKEN) > 1
+    error_message = "Missing Linode Key file path"
+  }
+}
+
+variable "VAULT_MONO_LOCAL_FILE_ROOT_CA_PASSPHRASE_PRIVATE_KEY" {
+  type    = string
+  default = null
+  validation {
+    condition     = length(var.VAULT_MONO_LOCAL_FILE_ROOT_CA_PASSPHRASE_PRIVATE_KEY) > 1
+    error_message = "Missing Root CA Private key password file path"
+  }
+}
+
+variable "VAULT_MONO_LOCAL_FILE_ROOT_CA_PRIVATE_KEY" {
+  type    = string
+  default = null
+  validation {
+    condition     = length(var.VAULT_MONO_LOCAL_FILE_ROOT_CA_PRIVATE_KEY) > 1
+    error_message = "Missing Root CA Private key file path"
+  }
+}
+
+# variable "VAULT_MONO_LOCAL_FILE_ROOT_CA_CERTIFICATE" {
+#   type    = string
+#   default = null
+#   validation {
+#     condition     = length(var.VAULT_MONO_LOCAL_FILE_ROOT_CA_CERTIFICATE) > 1
+#     error_message = "Missing Root CA Certificate key file path"
+#   }
+# }
