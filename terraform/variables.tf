6a15ce807cf666fc5e9291b98f523ee767625a1a
arpan <me@arpanrec.com>
2022-10-09 19:55:00 +0530

    terraform var change

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,18 +17,9 @@ variable "VAULT_MONO_VAULT_ROOT_TOKEN" {
   }
 }
 
-variable "VAULT_MONO_LOCAL_FILE_ROOT_CA_CERTIFICATE" {
-  type    = string
-  default = "../files/prerequisite.root_ca_certificate.crt"
-  validation {
-    condition     = length(var.VAULT_MONO_LOCAL_FILE_ROOT_CA_CERTIFICATE) > 1
-    error_message = "Missing root CA certificate file path"
-  }
-}
-
 variable "VAULT_MONO_LOCAL_FILE_CLIENT_PRIVATE_KEY" {
   type    = string
-  default = "../files/secrets.client_private_key.key"
+  default = "../.tmp/mutual_tls_certs/secrets.client_private_key.key"
   validation {
     condition     = length(var.VAULT_MONO_LOCAL_FILE_CLIENT_PRIVATE_KEY) > 1
     error_message = "Missing vault mutual TLS auth private key file path"
@@ -37,27 +28,9 @@ variable "VAULT_MONO_LOCAL_FILE_CLIENT_PRIVATE_KEY" {
 
 variable "VAULT_MONO_LOCAL_FILE_CLIENT_CERTIFICATE" {
   type    = string
-  default = "../files/client_certificate_chain.crt"
+  default = "../.tmp/mutual_tls_certs/client_certificate_chain.crt"
   validation {
     condition     = length(var.VAULT_MONO_LOCAL_FILE_CLIENT_CERTIFICATE) > 1
     error_message = "Missing vault mutual TLS auth client certificate file path"
-  }
-}
-
-variable "VAULT_MONO_LOCAL_FILE_OPENPGP_MASTER_PUBLIC_KEY" {
-  type    = string
-  default = "../files/prerequisite.openpgp_master_public.asc"
-  validation {
-    condition     = length(var.VAULT_MONO_LOCAL_FILE_OPENPGP_MASTER_PUBLIC_KEY) > 1
-    error_message = "Missing master OPENPGP public key file path"
-  }
-}
-
-variable "VAULT_MONO_LOCAL_FILE_OPENSSH_MASTER_PUBLIC_KEY" {
-  type    = string
-  default = "../files/prerequisite.openssh_master_rsa_id.key.pub"
-  validation {
-    condition     = length(var.VAULT_MONO_LOCAL_FILE_OPENSSH_MASTER_PUBLIC_KEY) > 1
-    error_message = "Missing master OPENSSH public key file path"
   }
 }
--- terraform/variables_prerequisite.tf
+++ terraform/variables_prerequisite.tf
@@ -1,6 +1,6 @@
 variable "VAULT_MONO_LOCAL_FILE_OPENSSH_PRIVATE_KEY" {
   type    = string
-  default = "../files/secrets.prerequisite.openssh_rsa_id.key"
+  default = "../.tmp/prerequisite/secrets.openssh_rsa_id.key"
   validation {
     condition     = length(var.VAULT_MONO_LOCAL_FILE_OPENSSH_PRIVATE_KEY) > 1
     error_message = "Missing OPEN SSH Private Key file path"
@@ -9,7 +9,7 @@ variable "VAULT_MONO_LOCAL_FILE_OPENSSH_PRIVATE_KEY" {
 
 variable "VAULT_MONO_LOCAL_FILE_OPENSSH_PUBLIC_KEY" {
   type    = string
-  default = "../files/prerequisite.openssh_rsa_id.key.pub"
+  default = "../.tmp/prerequisite/openssh_rsa_id.key.pub"
   validation {
     condition     = length(var.VAULT_MONO_LOCAL_FILE_OPENSSH_PUBLIC_KEY) > 1
     error_message = "Missing OPEN SSH Public Key file path"
@@ -18,7 +18,7 @@ variable "VAULT_MONO_LOCAL_FILE_OPENSSH_PUBLIC_KEY" {
 
 variable "VAULT_MONO_LOCAL_FILE_LINODE_CLI_PROD_TOKEN" {
   type    = string
-  default = "../files/secrets.prerequisite.linode_cli_prod_token.txt"
+  default = "../.tmp/prerequisite/secrets.linode_cli_prod_token.txt"
   validation {
     condition     = length(var.VAULT_MONO_LOCAL_FILE_LINODE_CLI_PROD_TOKEN) > 1
     error_message = "Missing Linode Key file path"
@@ -27,25 +27,16 @@ variable "VAULT_MONO_LOCAL_FILE_LINODE_CLI_PROD_TOKEN" {
 
 variable "VAULT_MONO_LOCAL_FILE_ROOT_CA_PRIVATE_KEY" {
   type    = string
-  default = "../files/secrets.prerequisite.root_ca_private_key.key"
+  default = "../.tmp/prerequisite/secrets.root_ca_private_key.key"
   validation {
     condition     = length(var.VAULT_MONO_LOCAL_FILE_ROOT_CA_PRIVATE_KEY) > 1
     error_message = "Missing Root CA Private key file path"
   }
 }
 
-# variable "VAULT_MONO_LOCAL_FILE_ROOT_CA_CERTIFICATE" {
-#   type    = string
-#   default = null
-#   validation {
-#     condition     = length(var.VAULT_MONO_LOCAL_FILE_ROOT_CA_CERTIFICATE) > 1
-#     error_message = "Missing Root CA Certificate key file path"
-#   }
-# }
-
 variable "VAULT_MONO_LOCAL_FILE_GITLAB_USERNAME" {
   type    = string
-  default = "../files/prerequisite.gitlab_username.txt"
+  default = "../.tmp/prerequisite/gitlab_username.txt"
   validation {
     condition     = length(var.VAULT_MONO_LOCAL_FILE_GITLAB_USERNAME) > 1
     error_message = "Missing Gitlab Username file path"
@@ -54,7 +45,7 @@ variable "VAULT_MONO_LOCAL_FILE_GITLAB_USERNAME" {
 
 variable "VAULT_MONO_LOCAL_FILE_GITLAB_GL_PROD_API_KEY" {
   type    = string
-  default = "../files/secrets.prerequisite.gitlab_gl_prod_api_key.txt"
+  default = "../.tmp/prerequisite/secrets.gitlab_gl_prod_api_key.txt"
   validation {
     condition     = length(var.VAULT_MONO_LOCAL_FILE_GITLAB_GL_PROD_API_KEY) > 1
     error_message = "Missing Gitlab API Key file path"
@@ -63,7 +54,7 @@ variable "VAULT_MONO_LOCAL_FILE_GITLAB_GL_PROD_API_KEY" {
 
 variable "VAULT_MONO_LOCAL_FILE_GITHUB_USERNAME" {
   type    = string
-  default = "../files/prerequisite.github_username.txt"
+  default = "../.tmp/prerequisite/github_username.txt"
   validation {
     condition     = length(var.VAULT_MONO_LOCAL_FILE_GITHUB_USERNAME) > 1
     error_message = "Missing Github Username file path"
@@ -72,7 +63,7 @@ variable "VAULT_MONO_LOCAL_FILE_GITHUB_USERNAME" {
 
 variable "VAULT_MONO_LOCAL_FILE_GITHUB_GH_PROD_API_TOKEN" {
   type    = string
-  default = "../files/secrets.prerequisite.github_gh_prod_api_key.txt"
+  default = "../.tmp/prerequisite/secrets.github_gh_prod_api_token.txt"
   validation {
     condition     = length(var.VAULT_MONO_LOCAL_FILE_GITHUB_GH_PROD_API_TOKEN) > 1
     error_message = "Missing Github API Key file path"
@@ -81,7 +72,7 @@ variable "VAULT_MONO_LOCAL_FILE_GITHUB_GH_PROD_API_TOKEN" {
 
 variable "VAULT_MONO_LOCAL_FILE_ANSIBLE_GALAXY_API_KEY" {
   type    = string
-  default = "../files/secrets.prerequisite.ansible_galaxy_api_key.txt"
+  default = "../.tmp/prerequisite/secrets.ansible_galaxy_api_key.txt"
   validation {
     condition     = length(var.VAULT_MONO_LOCAL_FILE_ANSIBLE_GALAXY_API_KEY) > 1
     error_message = "Missing Ansible Galaxy API Key file path"
@@ -90,9 +81,36 @@ variable "VAULT_MONO_LOCAL_FILE_ANSIBLE_GALAXY_API_KEY" {
 
 variable "VAULT_MONO_LOCAL_FILE_TERRAFORM_CLOUD_TF_PROD_TOKEN" {
   type    = string
-  default = "../files/secrets.prerequisite.terraform_cloud_tf_prod_token.txt"
+  default = "../.tmp/prerequisite/secrets.terraform_cloud_tf_prod_token.txt"
   validation {
     condition     = length(var.VAULT_MONO_LOCAL_FILE_TERRAFORM_CLOUD_TF_PROD_TOKEN) > 1
     error_message = "Missing Terraform API Key file path"
+  }
+}
+
+variable "VAULT_MONO_LOCAL_FILE_ROOT_CA_CERTIFICATE" {
+  type    = string
+  default = "../.tmp/prerequisite/root_ca_certificate.crt"
+  validation {
+    condition     = length(var.VAULT_MONO_LOCAL_FILE_ROOT_CA_CERTIFICATE) > 1
+    error_message = "Missing root CA certificate file path"
+  }
+}
+
+variable "VAULT_MONO_LOCAL_FILE_OPENPGP_MASTER_PUBLIC_KEY" {
+  type    = string
+  default = "../.tmp/prerequisite/openpgp_master_public.asc"
+  validation {
+    condition     = length(var.VAULT_MONO_LOCAL_FILE_OPENPGP_MASTER_PUBLIC_KEY) > 1
+    error_message = "Missing master OPENPGP public key file path"
+  }
+}
+
+variable "VAULT_MONO_LOCAL_FILE_OPENSSH_MASTER_PUBLIC_KEY" {
+  type    = string
+  default = "../.tmp/prerequisite/openssh_master_rsa_id.key.pub"
+  validation {
+    condition     = length(var.VAULT_MONO_LOCAL_FILE_OPENSSH_MASTER_PUBLIC_KEY) > 1
+    error_message = "Missing master OPENSSH public key file path"
   }
 }
