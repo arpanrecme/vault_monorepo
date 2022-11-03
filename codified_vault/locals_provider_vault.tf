971a153d99a36c6f307238bc64d3ce3af943cc9c
arpan <arpan.rec@gmail.com>
2022-11-03 18:40:19 +0530

    json changes

--- codified_vault/locals_provider_vault.tf
+++ codified_vault/locals_provider_vault.tf
@@ -2,8 +2,8 @@ locals {
 
   vault_mono_global_config = jsondecode(file(var.VAULT_MONO_LOCAL_FILE_GLOBAL_CONFIG))
 
-  vault_mono_vault_addr = format("https://%s:%s",
-    local.vault_mono_global_config.VAULT_ADDR_DOMAIN_NAME,
-  local.vault_mono_global_config.VAULT_ADDR_PORT)
+  vault_mono_vault_addr = format("%s://%s:%s",local.vault_mono_global_config.VAULT.PROTOCOL,
+    local.vault_mono_global_config.VAULT.FQDN,
+  local.vault_mono_global_config.VAULT.PORT)
 
 }
