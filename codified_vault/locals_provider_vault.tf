cf6cbbace936273b2ff6b82d7206403371a2361b
arpan <arpan.rec@gmail.com>
2022-11-03 19:13:57 +0530

    fix formatting

--- codified_vault/locals_provider_vault.tf
+++ codified_vault/locals_provider_vault.tf
@@ -2,7 +2,7 @@ locals {
 
   vault_mono_global_config = jsondecode(file(var.VAULT_MONO_LOCAL_FILE_GLOBAL_CONFIG))
 
-  vault_mono_vault_addr = format("%s://%s:%s",local.vault_mono_global_config.VAULT.PROTOCOL,
+  vault_mono_vault_addr = format("%s://%s:%s", local.vault_mono_global_config.VAULT.PROTOCOL,
     local.vault_mono_global_config.VAULT.FQDN,
   local.vault_mono_global_config.VAULT.PORT)
 
