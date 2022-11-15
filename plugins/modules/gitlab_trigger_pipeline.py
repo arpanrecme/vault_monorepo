fe331d71e460721b9494b5d96f4e3cabb643f8b6
arpan <arpan.rec@gmail.com>
2022-11-16 00:00:36 +0530

    gitlab_trigger_pipeline

---
+++ plugins/modules/gitlab_trigger_pipeline.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+
+# Copyright: (c) 2022, Arpan Mandal <arpan.rec@gmail.com>
+# MIT (see LICENSE or https://en.wikipedia.org/wiki/MIT_License)
+from __future__ import absolute_import, division, print_function
+
+import requests
+import urllib.parse
+from ansible.module_utils.basic import AnsibleModule
+import json
+__metaclass__ = type
+
+
+DOCUMENTATION = r'''
+---
+module: gitlab_trigger_pipeline
+
+short_description: Trigger Gitlab Pipeline
+
+version_added: "1.0.0"
+
+description: Trigger Gitlab Pipeline.
+
+options:
+    api_ep:
+        description: Rest Api endpoint
+        required: false
+        type: str
+        default: "https://gitlab.com"
+    private_token:
+        description: Gitlab Private Token.
+        required: false
+        type: str
+    token:
+        description: The trigger token or CI/CD job token.
+        required: false
+        type: str
+    project_id:
+        description: The ID or URL-encoded path of the project owned by the authenticated user.
+    ref:
+        description:
+            - The branch or tag to run the pipeline on.
+            - Defaults to the default branch of repo
+        required: false
+        type: str
+author:
+    - Arpan Mandal (mailto:arpan.rec@gmail.com)
+'''
+
+
+def crud(
+        api_ep=None,
+        private_token=None,
+        token=None,
+        project_id=None,
+        ref=None,
+) -> dict:
+    result = {"changed": False, "token_created": False}
+    _token_description = "gitlab_trigger_pipeline_tmp"
+
+    if not api_ep:
+        result["error"] = "Missing API Endpoint"
+        return result
+
+    if not private_token and not token:
+        result["error"] = "private_token or token is required"
+        return result
+
+    if private_token and token and ref:
+        result["error"] = "private_token and token are mutually exclusive when ref is present"
+        return result
+
+    if not project_id:
+        result["error"] = "project_id is mandatory"
+        return result
+
+    if not private_token and not ref:
+        result["error"] = "private_token is mandatory when ref is not present"
+        return result
+
+    project_id = urllib.parse.quote(project_id.encode("utf-8"),  safe='').strip()
+
+    if not token:
+        head = {
+            "PRIVATE-TOKEN": private_token
+        }
+        trigger_url = f"{api_ep}/api/v4/projects/{project_id}/triggers"
+        list_of_trigger_token_response = requests.get(trigger_url, timeout=30, headers=head)
+        if list_of_trigger_token_response.status_code == 200:
+            _if_token_exists = False
+            for token_details in list_of_trigger_token_response.json():
+                if token_details["description"] == _token_description:
+                    _token_details = token_details
+                    _if_token_exists = True
+                    break
+            if not _if_token_exists:
+                params = {
+                    "description": _token_description,
+                }
+                new_trigger_token_response = requests.post(
+                    trigger_url,
+                    timeout=30,
+                    headers=head,
+                    params=params
+                )
+                if new_trigger_token_response.status_code == 201:
+                    result["changed"] = True
+                    result["token_created"] = True
+                    _token_details = (new_trigger_token_response.json())
+                else:
+                    result["error"] = {
+                        "msg": new_trigger_token_response.json(),
+                        "status_code": new_trigger_token_response.status_code
+                    }
+                    return result
+        else:
+            result["error"] = {
+                "msg": list_of_trigger_token_response.json(),
+                "status_code": list_of_trigger_token_response.status_code
+            }
+            return result
+        token = _token_details["token"]
+    result["token"] = token
+
+    if not ref:
+        head = {
+            "PRIVATE-TOKEN": private_token
+        }
+        ref_url = f"{api_ep}/api/v4/projects/{project_id}"
+        ref_details_res = requests.get(ref_url, timeout=30, headers=head)
+        if ref_details_res.status_code == 200:
+            ref = ref_details_res.json().get("default_branch")
+        else:
+            result["error"] = {
+                "msg": ref_details_res.json(),
+                "status_code": ref_details_res.status_code
+            }
+            return result
+
+    result["ref"] = ref
+
+    trigger_pipeline_url = f"{api_ep}/api/v4/projects/{project_id}/trigger/pipeline"
+    params = {
+        "ref": ref,
+        "token": token
+    }
+    trigger_pipeline_details_res = requests.post(trigger_pipeline_url, params=params, timeout=30)
+    if trigger_pipeline_details_res.status_code == 201:
+        result["run_details"] = trigger_pipeline_details_res.json()
+        result["changed"] = True
+    else:
+        result["error"] = {
+            "msg": trigger_pipeline_details_res.json(),
+            "status_code": trigger_pipeline_details_res.status_code
+        }
+        return result
+    return result
+
+
+if __name__ == '__main__':
+    res = crud(
+        api_ep="https://gitlab.com",
+        private_token="",
+        project_id="arpanrecme/test",
+    )
+
+    print(json.dumps(res, indent=4))
